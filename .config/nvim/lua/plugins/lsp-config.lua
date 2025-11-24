e07c6a21fe5a5170f37abb119c2864ad9b9ce877
Diego Petrucci <baulei@icloud.com>
2025-11-24 19:30:46 +0000

    nvim: add workspace diagnostics for all lsps

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,79 +1,83 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- registry https://mason-registry.dev/registry/list
-        "copilot-language-server",
-        "lua-language-server",
-        "kotlin-lsp",
-        "marksman",
-        "pyright",
-        "ruff",
-        "swiftlint",
-        "taplo",
-        "yaml-language-server",
-        "xcbeautify"
-      },
-    },
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "copilot",
-        "lua_ls",
-        "kotlin_lsp",
-        "marksman",
-        "pyright",
-        "ruff",
-        "taplo",
-        "yamlls",
-      },
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- registry https://mason-registry.dev/registry/list
+				"copilot-language-server",
+				"lua-language-server",
+				"kotlin-lsp",
+				"marksman",
+				"pyright",
+				"ruff",
+				"swiftlint",
+				"taplo",
+				"yaml-language-server",
+				"xcbeautify",
+			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"copilot",
+				"lua_ls",
+				"kotlin_lsp",
+				"marksman",
+				"pyright",
+				"ruff",
+				"taplo",
+				"yamlls",
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"artemave/workspace-diagnostics.nvim",
+		},
+		config = function()
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						diagnostics = {
+							globals = { "Snacks" },
+						},
+					},
+				},
+			})
 
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            diagnostics = {
-              globals = { "Snacks" },
-            },
-          },
-        },
-      })
+			vim.lsp.config("sourcekit", {
+				-- Resolve sourcekit-lsp via active Xcode toolchain instead of relying on PATH.
+				cmd = { vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")) },
+			})
 
-      vim.lsp.config("sourcekit", {
-        -- Resolve sourcekit-lsp via active Xcode toolchain instead of relying on PATH.
-        cmd = { vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")) },
-      })
+			-- configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+			vim.lsp.enable({
+				"copilot",
+				"lua_ls",
+				"kotlin_lsp",
+				"marksman",
+				"pyright",
+				"ruff",
+				"sourcekit",
+				"taplo",
+				"yamlls",
+			})
 
-      -- configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-      vim.lsp.enable(
-        {
-          "copilot",
-          "lua_ls",
-          "kotlin_lsp",
-          "marksman",
-          "pyright",
-          "ruff",
-          "sourcekit",
-          "taplo",
-          "yamlls",
-        }
-      )
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code action" })
-    end,
-  }
+			for _, client in ipairs(vim.lsp.get_clients()) do
+				require("workspace-diagnostics").populate_workspace_diagnostics(client, 0)
+			end
+		end,
+	},
 }
