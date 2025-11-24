196421d4516b8af62df4da8874e71f5c3927b383
Diego Petrucci <baulei@icloud.com>
2025-11-24 19:34:07 +0000

    nvim: blink for autocompletion

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,47 +1,77 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = {
-    'rafamadriz/friendly-snippets',
-    "fang2hou/blink-copilot",
-  },
-  version = '1.*',
-  opts = {
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = {
-      preset = 'super-tab',
-    },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    completion = { documentation = { auto_show = true } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-copilot",
-          score_offset = 100,
-          async = true,
-        },
-        snippets = {
-          opts = {
-            friendly_snippets = true,
-            extended_filetypes = {
-              -- https://github.com/rafamadriz/friendly-snippets/tree/main/snippets
-              markdown = { 'jekyll' },
-              python = { 'django' },
-            }
-          }
-        }
-      }
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"fang2hou/blink-copilot",
+	},
+	version = "1.*",
+	opts = {
+		keymap = {
+			-- https://cmp.saghen.dev/configuration/keymap
+			preset = "super-tab",
+			["<S-p>"] = { "select_prev", "fallback" },
+			["<S-n>"] = { "select_next", "fallback" },
+			["<S-u>"] = { "scroll_signature_up", "fallback" },
+			["<S-d>"] = { "scroll_signature_down", "fallback" },
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 0,
+			},
+			menu = {
+				auto_show = true,
+				draw = {
+					treesitter = { "lsp" },
+					columns = {
+						{
+							"kind_icon",
+							"label",
+							gap = 1,
+						},
+						{
+							"kind",
+						},
+					},
+				},
+			},
+		},
+		signature = {
+			enabled = true,
+		},
+		sources = {
+			default = {
+				"lsp",
+				"path",
+				"snippets",
+				"buffer",
+				"copilot",
+			},
+			providers = {
+				copilot = {
+					name = "copilot",
+					module = "blink-copilot",
+					score_offset = 100,
+					async = true,
+				},
+				snippets = {
+					opts = {
+						friendly_snippets = true,
+						extended_filetypes = {
+							-- snippets for frameworks https://github.com/rafamadriz/friendly-snippets/tree/main/snippets/frameworks
+							markdown = { "jekyll" },
+							python = { "django" },
+						},
+					},
+				},
+			},
+		},
+		fuzzy = {
+			implementation = "prefer_rust_with_warning",
+		},
+	},
+	opts_extend = { "sources.default" },
 }
