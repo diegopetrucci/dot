80141684f6fafa71b458e5e317e5a5c0f39aa47d
Diego Petrucci <baulei@icloud.com>
2025-12-18 21:19:02 +0000

    add ios dev stuff to nvim lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,24 @@
+local function xcodebuild_device()
+	if vim.g.xcodebuild_platform == "macOS" then
+		return " macOS"
+	end
+
+	local deviceIcon = ""
+	if vim.g.xcodebuild_platform:match("watch") then
+		deviceIcon = "􀟤"
+	elseif vim.g.xcodebuild_platform:match("tv") then
+		deviceIcon = "􀡴 "
+	elseif vim.g.xcodebuild_platform:match("vision") then
+		deviceIcon = "􁎖 "
+	end
+
+	if vim.g.xcodebuild_os then
+		return deviceIcon .. " " .. vim.g.xcodebuild_device_name .. " (" .. vim.g.xcodebuild_os .. ")"
+	end
+
+	return deviceIcon .. " " .. vim.g.xcodebuild_device_name
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -57,6 +78,27 @@ return {
 						readonly = "",
 						unnamed = "",
 						newfile = "",
+					},
+				},
+				{
+					"' ' .. vim.g.xcodebuild_last_status",
+					color = {
+						fg = secondary_fg,
+						bg = bg,
+					},
+				},
+				{
+					xcodebuild_device,
+					color = {
+						fg = secondary_fg,
+						bg = bg,
+					},
+				},
+				{
+					"'󰙨 ' .. vim.g.xcodebuild_test_plan",
+					color = {
+						fg = secondary_fg,
+						bg = bg,
 					},
 				},
 				{
