8bdd249a606e575f9bfc2f421fa8909197cd2a0d
Diego Petrucci <baulei@icloud.com>
2025-12-18 21:19:47 +0000

    disable pyright per-project with an untracked file

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -63,18 +63,36 @@ return {
 				-- end,
 			})
 
-			-- configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-			vim.lsp.enable({
+			-- Helper function to check if pyright should be enabled
+			local function should_enable_pyright()
+				local root_markers = { "pyproject.toml", "setup.py", "requirements.txt", ".git" }
+				local root_dir = vim.fs.root(0, root_markers)
+
+				if root_dir and vim.fn.filereadable(root_dir .. "/.disable-pyright") == 1 then
+					return false
+				end
+				return true
+			end
+
+			-- Build LSP list conditionally
+			local lsp_servers = {
 				"copilot",
 				"lua_ls",
 				"kotlin_lsp",
 				"marksman",
-				"pyright",
 				"ruff",
 				"sourcekit",
 				"taplo",
 				"yamlls",
-			})
+			}
+
+			-- Only add pyright if not disabled
+			if should_enable_pyright() then
+				table.insert(lsp_servers, 5, "pyright") -- Insert after marksman
+			end
+
+			-- configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+			vim.lsp.enable(lsp_servers)
 
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 
