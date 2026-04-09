0ea48a1ffbc028ca9ee78523afec6ca96a9c2229
Diego Petrucci <baulei@icloud.com>
2026-04-09 07:57:26 +0100

    neovim partial migration to vimpack

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,5 +16,20 @@ vim.opt.rtp:prepend(lazypath)
 require("keymaps")
 require("options")
 
+-- vim.pack plugins must be added before lazy.setup() because lazy resets packpath
+-- to only $VIMRUNTIME, which causes :packadd to fail with E919 afterwards
+vim.pack.add({
+	'https://github.com/tpope/vim-obsession',
+	'https://github.com/ThePrimeagen/vim-be-good',
+	'https://github.com/mluders/comfy-line-numbers.nvim',
+	'https://github.com/nvim-mini/mini.nvim',
+	'https://github.com/rafamadriz/friendly-snippets',
+	'https://github.com/norcalli/nvim-colorizer.lua',
+	'https://github.com/m4xshen/hardtime.nvim',
+})
+
+vim.cmd.packadd('hardtime.nvim')
+require('hardtime').setup()
+
 -- start lazy
 require("lazy").setup("plugins")
