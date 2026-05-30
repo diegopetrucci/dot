d898a021fdde9626827dedf95020ad7faf637619
Diego Petrucci <baulei@icloud.com>
2026-05-30 14:13:49 +0100

    more zsh

--- .config/zsh/09-source.zsh
+++ .config/zsh/09-source.zsh
@@ -1,6 +1,5 @@
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history)
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /opt/homebrew/share/zsh-you-should-use/you-should-use.plugin.zsh
 
 source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
