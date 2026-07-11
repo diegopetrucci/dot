cd83af547b6f616307702025744a90043fa51a97
Diego Petrucci <baulei@icloud.com>
2026-07-11 09:08:27 +0100

    direnv zsh

--- .config/zsh/14-direnv.zsh
+++ .config/zsh/14-direnv.zsh
@@ -1,1 +1,4 @@
-eval "$(direnv hook zsh)"
+# Silence direnv's load/unload messages so Powerlevel10k instant prompt
+# doesn't warn about console output during shell initialization.
+export DIRENV_LOG_FORMAT=''
+(( $+commands[direnv] )) && eval "$(direnv hook zsh)"
